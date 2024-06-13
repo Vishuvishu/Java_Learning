90919cccd27bbbe793dea0e128d022329bce9a21
Vishuvishu <98393037+Vishuvishu@users.noreply.github.com>
2024-06-13 11:43:40 +0530

    jobportal project

---
+++ java_project_spring/JobPortal/UISpringMongodb-main/src/pages/Home.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import "./home.css"
+
+const Home = () => {
+  return (
+    <div className="background">
+      <div className="overlay">
+        <Typography sx={{ margin: "5%" }} variant="h3" align="center">
+          Get Hired or Hire people for free!
+        </Typography>
+        <div>
+          <ul className="ul">
+            <li>
+              <button class="button-52" role="button"><Link to="/employer/dashboard">
+                Hire talent
+              </Link></button>
+
+
+            </li>
+            <li>
+              <button class="button-52" role="button"> <Link to="/employee/feed">
+                Get Job Now
+              </Link></button>
+              {/* 
+              <Button sx={{ margin: "2% 3%" }} variant="outlined">
+               
+              </Button> */}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
